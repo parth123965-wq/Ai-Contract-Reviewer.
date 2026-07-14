6035e59486cf8e79e2f4d628441726e26a722afd
parth123965-wq <parth123965@gmail.com>
2026-07-14 18:16:08 +0530

    Implement database connection setup with SQLAlchemy

---
+++ app/database/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker , declarative_base
+from app.core.config import settings
+
+engine = create_engine(
+    url=settings.DATABASE_URL
+)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
